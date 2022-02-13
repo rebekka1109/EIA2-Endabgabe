bf71acc0fb2ff0979d43a5ebf8d98367293dfe11
rebekka1109 <80899236+rebekka1109@users.noreply.github.com>
2022-02-13 15:28:33 +0100

    Add files via upload

--- Vektor.js
+++ Vektor.js
@@ -1,1 +1,29 @@
+var Abschlussarbeit;
+(function (Abschlussarbeit) {
+    var Vector = /** @class */ (function () {
+        function Vector(_x, _y) {
+            this.set(_x, _y);
+        }
+        Vector.getDifference = function (_v0, _v1) {
+            return new Vector(_v0.x - _v1.x, _v0.y - _v1.y);
+        };
+        Vector.prototype.set = function (_x, _y) {
+            this.x = _x;
+            this.y = _y;
+        };
+        Vector.prototype.scale = function (_factor) {
+            this.x *= _factor;
+            this.y *= _factor;
+        };
+        Vector.prototype.add = function (_addend) {
+            this.x += _addend.x;
+            this.y += _addend.y;
+        };
+        Vector.prototype.copy = function () {
+            return new Vector(this.x, this.y);
+        };
+        return Vector;
+    }());
+    Abschlussarbeit.Vector = Vector;
+})(Abschlussarbeit || (Abschlussarbeit = {}));
 //# sourceMappingURL=Vektor.js.map
