1b91f04e45463ee4bfa7ec4539c70fad9e8875c0
rebekka1109 <80899236+rebekka1109@users.noreply.github.com>
2022-02-14 17:48:51 +0100

    Add files via upload

--- Vektor.js
+++ Vektor.js
@@ -7,6 +7,14 @@ var Abschlussarbeit;
         Vector.getDifference = function (_v0, _v1) {
             return new Vector(_v0.x - _v1.x, _v0.y - _v1.y);
         };
+        Vector.getRandom = function (_minLength, _maxLength) {
+            var vector = new Vector(0, 0);
+            var length = _minLength + Math.random() * (_maxLength - _minLength);
+            var direction = Math.random() * 2 * Math.PI;
+            vector.set(Math.cos(direction), Math.sin(direction));
+            vector.scale(length);
+            return vector;
+        };
         Vector.prototype.set = function (_x, _y) {
             this.x = _x;
             this.y = _y;
--- Vektor.ts
+++ Vektor.ts
@@ -1,5 +1,5 @@
 namespace Abschlussarbeit {
-    export class Vector{
+    export class Vector {
         public x: number;
         public y: number;
 
@@ -9,6 +9,16 @@ namespace Abschlussarbeit {
 
         public static getDifference(_v0: Vector, _v1: Vector): Vector {
             return new Vector(_v0.x - _v1.x, _v0.y - _v1.y);
+        }
+
+        public static getRandom(_minLength: number, _maxLength: number): Vector {
+            let vector: Vector =  new Vector(0, 0);
+            let length: number = _minLength + Math.random() * (_maxLength - _minLength);
+            let direction: number = Math.random() * 2 * Math.PI;
+
+            vector.set(Math.cos(direction), Math.sin(direction));
+            vector.scale(length);
+            return vector;
         }
 
         public set(_x: number, _y: number): void {
